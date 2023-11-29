a08240aec27d95e2526c24910811b769203d9746
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-29 14:08:58 +0300

    Challenge done

--- video-game-button/App.jsx
+++ video-game-button/App.jsx
@@ -1,10 +1,9 @@
-import React from "react"
+import React from "react";
 
 export default function App() {
-  
-  const [gameRunning, setGameRunning] = React.useState(false)
-  
-/* Challenge
+  const [gameRunning, setGameRunning] = React.useState(false);
+
+  /* Challenge
   
     You're creating a button that will allow the user to play and pause a video game. Your task is to set it up and insert it in the div below (line 29) as follows: 
             
@@ -25,9 +24,16 @@ export default function App() {
         4. If you complete these tasks correctly, the button should have some nice visual effects 
            when you click it, and the workshop background should be automatically replaced by a light blue background.
 */
-    return (
-        <div>       
 
-        </div>
-  )
+  const gameBtnHandler = () => {
+    setGameRunning((prevVal) => !prevVal);
+  };
+
+  return (
+    <div>
+      <button className="video-game-button" onClick={gameBtnHandler}>
+        {gameRunning ? "Play" : "Pause"}
+      </button>
+    </div>
+  );
 }
