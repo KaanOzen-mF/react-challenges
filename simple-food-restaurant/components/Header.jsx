9a1f69874e6eb8277f35f40f7cfa4628b74ae05f
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-13 16:08:23 +0300

    Header main and footer components add

---
+++ simple-food-restaurant/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="button-container">
+          <button>Our Menu</button>
+          <button>Abort</button>
+          <button>Hours & Location</button>
+        </div>
+      </header>
+    </>
+  );
+}
