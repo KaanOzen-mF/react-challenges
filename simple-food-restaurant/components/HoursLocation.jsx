a7580606938c857824684147e4c44c4a9da12ed8
KaanOzen-mF <kaanozn98@gmail.com>
2023-12-14 11:00:17 +0300

    Hours and Location components done

--- simple-food-restaurant/components/HoursLocation.jsx
+++ simple-food-restaurant/components/HoursLocation.jsx
@@ -3,7 +3,22 @@ import React from "react";
 export default function HoursLocation() {
   return (
     <>
-      <p>Hours Location</p>
+      <main>
+        <div className="info-container">
+          {" "}
+          <p>Restaurant Name: [Your Restaurant Name Here] </p>
+          <p>Location: 123 Main Street, Hometown, State, Zip Code </p>
+          <p>
+            Hours of Operation: Monday to Thursday: 11:00 AM - 9:00 PM Friday
+            and Saturday: 11:00 AM - 10:00 PM Sunday: Closed
+          </p>
+          <p>
+            Contact Us: Phone: (123) 456-7890 Email: info@yourrestaurant.com
+            Find Us Online: Website: www.yourrestaurant.com Facebook:
+            /YourRestaurant Instagram: @YourRestaurant
+          </p>
+        </div>
+      </main>
     </>
   );
 }
